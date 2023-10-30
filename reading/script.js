124ed0108cf4ae8db6eb9c5ae3cf10a7b1e5fbf2
Wallenborg <niklas.wallenborg@hyperisland.se>
2023-10-30 09:13:56 +0100

    add javascript

---
+++ reading/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var shuffleLink = document.getElementById("shuffle-link");
+
+  shuffleLink.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Lägg till din kod för att ändra klasserna för dina kort här
+    document.querySelector(".card-0").style.animation =
+      "shuffle 0.5s 4 backwards";
+    document.querySelector(".card-1").style.animation =
+      "shuffle 0.8s 4 backwards";
+    document.querySelector(".card-3").style.animation = "shuffle 1s 2 linear";
+    document.querySelector(".card-4").style.animation = "shuffle 0.8s 4 linear";
+
+    var delay = 4600;
+
+    setTimeout(function () {
+      window.location.href = "past.html";
+    }, delay);
+  });
+});
